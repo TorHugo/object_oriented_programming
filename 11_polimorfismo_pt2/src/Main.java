b8dee2ea15955a1123202336c96356aaab4119de
Victor Hugo de Arruda Jesus <88560162+TorHugo@users.noreply.github.com>
2022-06-01 00:38:15 -0300

    Object Oriented Programming Polimorfismo.

--- 11_polimorfismo_pt2/src/Main.java
+++ 11_polimorfismo_pt2/src/Main.java
@@ -1,2 +1,14 @@
 public class Main {
+    public static void main(String[] args) {
+        Lobo lobo = new Lobo();
+        Cachorro cachorro = new Cachorro();
+
+        lobo.emitirSom();
+        cachorro.emitirSom();
+
+        cachorro.reagir("Olá");
+        cachorro.reagir(true);
+        cachorro.reagir(6, 20.5f);
+        cachorro.reagir(8,30);
+    }
 }
